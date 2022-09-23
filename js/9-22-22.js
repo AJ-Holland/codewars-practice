a0888a4f91bde9ad25a2d3a039274e5175da407b
AJ Holland <aholland95@gmail.com>
2022-09-22 22:33:45 -0500

    code challenge 9-22-22

---
+++ js/9-22-22.js
@@ -0,0 +1,10 @@
+// 9-22-22
+// Write a function which converts the input string to uppercase.
+// describe("Basic Tests", function(){
+//     it("should pass the basic tests", function(){
+//       Test.assertEquals(makeUpperCase("hello"), "HELLO");
+//     });
+//   });
+function makeUpperCase(str) {
+    return str.toUpperCase()
+  }
